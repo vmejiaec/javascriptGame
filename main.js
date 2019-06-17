3f4fdea3eeee2d2c83dda1691d81ac62547dce2f
vmejiaec <vmejiaec@gmail.com>
2019-06-16 19:55:00 -0500

    Ya dibujo la bactaria mini

--- main.js
+++ main.js
@@ -27,7 +27,65 @@ function dibujaPuntos(){
     var width = canvas.width;
     var height = canvas.height;
     // dibuja un rectangulo
-    ctx.strokeStyle = "red";
-    ctx.strokeRect(10,20,2,2);
+    ctx.strokeStyle = "red"; 
 
+    var xo = 10;
+    var yo = 30;
+    
+    var posini = [10,30];
+
+    var bacteria = arrayBacteria();
+
+    ctx.beginPath();
+    
+    trazarBacteria(bacteria, ctx, posini, 2);
+
+    posini[0] += 15;
+
+    trazarBacteria(bacteria, ctx, posini, 2);
+
+    posini[0] += 15;
+
+    trazarBacteria(bacteria, ctx, posini, 2);
+
+    ctx.stroke();
+}
+
+function arrayBacteria(){
+    var bacteria = [
+        ["move",2,3],
+        ["line",2,2],
+        ["line",7,2],
+        ["line",8,3],
+        ["line",8,7],
+        ["line",7,8],
+        ["line",2,8],
+        ["line",2,7],
+        ["line",1,6],
+        ["line",1,4],
+        ["line",2,3],
+        ["move",6.5,4],
+        ["line",7,4.5],
+        ["move",7,5.5],
+        ["line",6.5,6],
+        ["move",0,5],
+        ["line",1,5]
+    ];
+    return bacteria;
+}
+
+function trazarBacteria(bacteria, ctx, posini , factor){
+    for (var i=0 ; i<bacteria.length; i++){
+        var tipo = bacteria[i][0];
+        var xb = bacteria[i][1];
+        var yb = bacteria[i][2];
+        // factor de escala y posición inicial
+        xb = factor*xb + posini[0];
+        yb = factor*yb + posini[1];
+        // dibuja el trazo
+        if (tipo == "move")
+            ctx.moveTo(xb,yb);
+        if (tipo == "line")
+            ctx.lineTo(xb,yb);
+    }
 }
